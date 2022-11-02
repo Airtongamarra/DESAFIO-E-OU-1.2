c7a0dda899bf33c8fb00e809bb13c808533d6fd7
Airton Gamarra <airtongamarra575@gmail.com>
2022-11-02 15:12:02 -0300

    Repositorio

--- Desafio1.2.js
+++ Desafio1.2.js
@@ -1,3 +1,13 @@
+/*2. Escreva um algoritmo que faça as seguintes perguntas ao seu usuário, guarde as
+respostas separadamente:
+a. Se ele está com o cinto de segurança
+b. Se ele está alcoolizado
+c. Se o semáforo está verde
+Com base nas respostas do usuário informe que o usuário pode prosseguir se
+estiver com o cinto E não estiver alcoolizado E o semáforo estiver verde, se não
+informar que ele não deve prosseguir.*/
+
+
 var cinto = prompt(" voce esta com o sinto de segurança posto? 'S' ou 'N' ")
 var bebeu = prompt(" Voce consumiu algum tipo de bebida alcoolica? 'S' ou 'N' ")
 var semaforo = prompt( " O sinal esta verde ? 'S' ou 'N' ")
